4a1c7d7918148182c2c532038f3026b824681525
zygimantas <zygimantasdev@gmail.com>
2016-06-26 18:51:21 +0300

    added app name

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -13,7 +13,7 @@ export class ProjectConfig extends SeedConfig {
 
   constructor() {
     super();
-    // this.APP_TITLE = 'Put name of your app here';
+    this.APP_TITLE = 'Youtube search and ng2 gonna be good';
     let additional_deps: InjectableDependency[] = [
       // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
