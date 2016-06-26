d9465c2cb53fd5a52631fa2079a6a8ca62a2152a
zygimantas <zygimantasdev@gmail.com>
2016-06-26 18:44:16 +0300

    search box element working

--- src/client/app/+youtube/youtube.component.ts
+++ src/client/app/+youtube/youtube.component.ts
@@ -1,6 +1,49 @@
-import {Component, provide } from '@angular/core';
-import { YoutubeService } from '../shared/index';
-import { SearchResult } from './youtube.models';
+import {Component, provide, OnInit, ElementRef} from '@angular/core';
+import {YoutubeService} from '../shared/index';
+import {SearchResult} from './youtube.models';
+import {EventEmitter} from "@angular/platform-browser-dynamic/src/facade/async";
+import {Observable} from "rxjs/Rx";
+
+@Component({
+  outputs: ['loading', 'results'],
+  selector: 'search-box',
+  template: `
+      <input type="text" class="form-control" placeholder="Search" autofocus>
+    `
+})
+class SearchBox implements OnInit {
+  loading:EventEmitter<boolean> = new EventEmitter<boolean>();
+  results:EventEmitter<SearchResult[]> = new EventEmitter<SearchResult[]>();
+
+  constructor(public youtubeService: YoutubeService,
+              private el: ElementRef) {
+  }
+
+  ngOnInit():any {
+    Observable
+      .fromEvent(this.el.nativeElement, 'keyup')
+      .map((e: any) => e.target.value)
+      .filter((text: string) => text.length > 1)
+      .debounceTime(250)
+      .do(() => this.loading.emit(true))
+      .map((query: string) => this.youtubeService.search(query))
+      .switch()
+      .subscribe(
+        (results: SearchResult[]) => {
+          this.loading.emit(false);
+          this.results.emit(results);
+        },
+        (error: any) => {
+          console.log(error);
+          this.loading.emit(false);
+        },
+        () => { // on completion
+          this.loading.emit(false);
+        }
+      )
+  }
+}
+
 
 /**
  * This class represents the lazy loaded YoutubeComponent.
@@ -10,7 +53,8 @@ import { SearchResult } from './youtube.models';
   selector: 'sd-youtube',
   templateUrl: 'youtube.component.html',
   styleUrls: ['youtube.component.css'],
-  providers: [YoutubeService]
+  providers: [YoutubeService],
+  directives: [SearchBox]
 })
 export class YoutubeComponent {
 
@@ -20,11 +64,9 @@ export class YoutubeComponent {
    *
    * @param youtubeService
    */
-  constructor(public youtubeService: YoutubeService) {
+  constructor(public youtubeService:YoutubeService) {
     youtubeService.search('korgi').subscribe((data) => {
       console.log(data);
     });
   }
-
-
 }
